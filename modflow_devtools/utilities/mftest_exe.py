b19b4cf5145d25e8319044c1bc4a3b77b03003dd
mjreno <mjreno@ncsu.edu>
2022-07-26 16:54:14 -0400

    testing devtools

--- modflow_devtools/utilities/mftest_exe.py
+++ modflow_devtools/utilities/mftest_exe.py
@@ -96,7 +96,7 @@ class MFTestExe:
         shutil.rmtree(self._releasebin, ignore_errors=True)
 
     def _create_dirs(self):
-        pths = [self._releasebin, self._working_dir]
+        pths = [self._releasebin, self._working_dir, os.path.join("..", "bin")]
         for pth in pths:
             print(f"creating... {os.path.abspath(pth)}")
             os.makedirs(pth, exist_ok=True)
